4a222714f8b3d30e4af02e8fd1849bf5bb495b5e
SaadHany99 <159723036+SaadHany99@users.noreply.github.com>
2024-06-07 22:58:07 +0300

    first commit

---
+++ ITI.Grpc.Server/Services/InventroyService.cs
@@ -0,0 +1,107 @@
+﻿using ITI.Grpc.Protos;
+using static ITI.Grpc.Protos.InventoryServiceProto;
+using Grpc.Core;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ITI.Grpc.Server.Services
+{
+    public class InventroyService : InventoryServiceProtoBase
+    {
+        public static List<Product> Products { get; set; }
+
+
+        public InventroyService()
+        {
+            Products = new List<Product>()
+        {
+            new Product{Id=1,Name="InterMilan",Price=400,Quantity=13 , Category= ProductCategory.Kits},
+            new Product{Id=2,Name="Chair",Price=10000,Quantity=5,  Category=ProductCategory.Furniture},
+            new Product{Id=3,Name="Cutter",Price=50,Quantity=7 ,  Category=ProductCategory.Tools},
+            new Product{Id=4,Name="Glue",Price=40,Quantity=16 , Category= ProductCategory.Tools},
+            new Product{Id=5,Name="Italia NT",Price=500,Quantity=15,  Category=ProductCategory.Kits},
+            new Product{Id=6,Name="Table",Price=12000,Quantity=3 ,  Category=ProductCategory.Furniture}
+        };
+
+        }
+        [Authorize(AuthenticationSchemes = Consts.ApiKeySchemeName)]
+
+        public override async Task<Products> GetAll(Empty request, ServerCallContext context)
+        {
+            var products = new Products();
+            products.Products_.AddRange(Products);
+            return await Task.FromResult(products); ;
+        }
+        [Authorize(AuthenticationSchemes = Consts.ApiKeySchemeName)]
+        public override async Task<IsExisted> GetProductById(Id request, ServerCallContext context)
+        {
+            var product = Products.FirstOrDefault(i => i.Id == request.Id_);
+            if (product != null)
+            {
+                return await Task.FromResult(new IsExisted
+                {
+                    IsExisted_ = true
+                }); ;
+            }
+            return await Task.FromResult(new IsExisted
+            {
+                IsExisted_ = false
+            }); ;
+            //return base.GetProductById(request, context);
+        }
+
+
+        [Authorize(AuthenticationSchemes = Consts.ApiKeySchemeName)]
+
+        public override async Task<Product> AddProduct(Product request, ServerCallContext context)
+        {
+            Products.Add(request);
+            return await Task.FromResult(request);
+
+        }
+
+
+        [Authorize(AuthenticationSchemes = Consts.ApiKeySchemeName)]
+
+        public override async Task<Product> UpdateProduct(Product request, ServerCallContext context)
+
+        {
+            var product = Products.FirstOrDefault(i => i.Id == request.Id);
+            if (product != null)
+            {
+                product.Name = request.Name;
+                product.Price = request.Price;
+                product.Quantity = request.Quantity;
+            }
+
+
+            return await Task.FromResult(product);
+        }
+        [Authorize(AuthenticationSchemes = Consts.ApiKeySchemeName)]
+
+        public override async Task<productsNumber> AddBulkProducts(IAsyncStreamReader<Product> requestStream, ServerCallContext context)
+        {
+            int count = 0;
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                Products.Add(request);
+                ++count;
+            }
+
+            return await Task.FromResult(new productsNumber { Count = count });
+
+        }
+        [Authorize(AuthenticationSchemes = Consts.ApiKeySchemeName)]
+
+        public override async Task GetProductReport(Empty request, IServerStreamWriter<Product> responseStream, ServerCallContext context)
+        {
+            foreach (var item in Products)
+            {
+                await responseStream.WriteAsync(item);
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
